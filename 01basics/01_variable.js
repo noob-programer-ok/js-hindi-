29093629cf8e48ace97430aa090928aabe469e2b
noob-programer-ok <ak552211s@gmail.com>
2025-08-15 10:05:24 +0000

    container config done

---
+++ 01basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountid = 1234556
+let accountEmail = "Akash@google.com"
+var password = "1234"
+accountcity = "Jaipur"
+
+//accountid = 222
+accountEmail = "Ak@Ak.com"
+password = "5678"
+accountcity = "Bengaluru"
+
+console.log(accountid);
+
+console.table([accountid,accountEmail,password,accountcity])
